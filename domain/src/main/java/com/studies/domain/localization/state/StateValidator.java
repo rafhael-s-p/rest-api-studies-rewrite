e3a4073e5ba678a754f095f36516e010bc9a1021
Rafhael <rafhael.dev@gmail.com>
2024-11-15 16:17:31 -0300

    feat: state validations

--- domain/src/main/java/com/studies/domain/localization/state/StateValidator.java
+++ domain/src/main/java/com/studies/domain/localization/state/StateValidator.java
@@ -6,6 +6,9 @@ import com.studies.domain.validation.Validator;
 
 public class StateValidator extends Validator {
 
+    public static final int NAME_MAX_LENGTH = 255;
+    public static final int NAME_MIN_LENGTH = 3;
+
     private final State state;
 
     protected StateValidator(final State state, final ValidationHandler aHandler) {
@@ -15,7 +18,23 @@ public class StateValidator extends Validator {
 
     @Override
     public void validate() {
-        if (this.state.getName() == null)
+        checkNameConstraints();
+    }
+
+    private void checkNameConstraints() {
+        final var name = this.state.getName();
+        if (name == null) {
             this.validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+
+        if (name.isBlank()) {
+            this.validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+
+        final int length = name.trim().length();
+        if (length > NAME_MAX_LENGTH || length < NAME_MIN_LENGTH)
+            this.validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
     }
 }
